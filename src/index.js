57a98381da2f46e0569f83c0065ba52168cf37d5
KirillFilipp <kirill_filipp@mail.ru>
2019-02-27 00:09:40 +0300

    feat: add decision task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
+  let count = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    let k = preferences[i];
+    let n = preferences[k - 1];
+    let t = preferences[n - 1];
+    if (i === t - 1 && k != t) {
+      count++;
+    }
+  }
+  return count / 3;
   // your implementation
 };
